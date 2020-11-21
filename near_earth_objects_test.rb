6cc7bba8b58990162e48de0f2d50d53e743ece47
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-20 22:53:43 -0700

    Refactor

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -5,7 +5,9 @@ require_relative 'near_earth_objects'
 
 class NearEarthObjectsTest < Minitest::Test
   def test_a_date_returns_a_list_of_neos
-    results = NearEarthObjects.find_neos_by_date('2019-03-30')
-    assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
+    neo = NearEarthObjects.new('2019-03-31')
+    results = neo.find_neos_by_date
+  
+    assert_equal '163081 (2002 AG29)', results[:astroid_list][0][:name]
   end
 end
